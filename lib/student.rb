00d569fe7b57e681cb90fef8f6c808aa3c1bdbe6
Alyssa Brown <alyssa.brown@live.com>
2019-03-27 02:05:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,15 @@ class Student
 attr_accessor :name, :grade 
 
 def initialize (name, grade, id=nil)
+    @id = id
+    @name = name
+    @grade = grade
+    
+  end 
+  
+  def self.create_table 
+    
+  end 
   
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
