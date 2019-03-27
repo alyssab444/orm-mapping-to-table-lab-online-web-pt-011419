41cd3173aaed76b2678b65182278d6e0f8de1f24
Alyssa Brown <alyssa.brown@live.com>
2019-03-27 02:12:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,8 +41,11 @@ def initialize (name, grade, id=nil)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
   
-  def self.create 
-    
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student 
+  end
   end 
   
 end
